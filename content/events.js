0d5c65a254d9cc356e28e74c29aa13571aa1b476
Apollo <cowak186@gmail.com>
2022-08-01 20:04:10 -0500

    added beggining of card to page

---
+++ content/events.js
@@ -0,0 +1,40 @@
+const server_id = '633521381593710602';
+var events;
+//get events from server
+
+//sort by date
+
+//make card for each
+// for (discordEvent in events) {
+//     let card = document.createElement('div');
+//     card.className = 'card';
+
+//     //add if event contains image
+//     let img = document.createElement("img");
+//     img.setAttribute('src', 'add event image here');
+
+//     let container = document.createElement('div');
+//     container.className = 'container';
+// }
+
+let card = document.createElement('div');
+card.className = 'card';
+document.body.childNodes.item(1).firstChild.firstChild.appendChild(card);
+
+//add if event contains image
+let img = document.createElement("img");
+img.setAttribute('src', '/generated/assets/borley.png');
+img.setAttribute('style','width: 100%');
+card.appendChild(img);
+
+let container = document.createElement('div');
+container.className = 'container';
+card.appendChild(container);
+
+let title = document.createElement('h1');
+title.innerText = "test";
+container.appendChild(title);
+
+let subtitle = document.createElement('p');
+subtitle.innerText = "subtitle";
+container.appendChild(subtitle);
