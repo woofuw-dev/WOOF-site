61f2256d74f3c3d0bdc2973ab51ec9c5767bd6f0
HyperionSkunk <cowak186@gmail.com>
2022-08-01 23:52:42 -0500

    Update content/frontimg.js
    Co-authored-by: by77er <by77er@gmail.com>

--- content/frontimg.js
+++ content/frontimg.js
@@ -1,5 +1,5 @@
 var imagesArray = ["barley_ac_1.png", "barley_ac_2.png", "barley_beach_1.png", "barley_mo_carbon_beach_1.png", "mo_beach_1.png", "mo_beach_2.png", "pride_beach_1.png", "sergio_barley_1.png", "twister_ac_1.png", "twister_ac_2.png", "twister_barley_ac_1.png"];
 
 const img = document.getElementById("splashimg");
-var num = Math.floor(Math.random() * 10); // 0...11
+var num = Math.floor(Math.random() * imagesArray.length); // select random image from list
 img.src = 'assets/img/carosel/' + imagesArray[num];
