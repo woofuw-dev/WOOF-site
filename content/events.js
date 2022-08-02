1be893657552b66d9cc1a38f1eface85e2dc91a5
HyperionSkunk <cowak186@gmail.com>
2022-08-02 00:44:43 -0500

    clean up base

--- content/events.js
+++ content/events.js
@@ -2,33 +2,21 @@ const server_id = '633521381593710602';
 var events;
 //get events from server
 
-//sort by date
-
-//make card for each
+// TODO: make new card for each event
 // for (discordEvent in events) {
-//     let card = document.createElement('div');
-//     card.className = 'card';
-
-//     //add if event contains image
-//     let img = document.createElement("img");
-//     img.setAttribute('src', 'add event image here');
-
-//     let container = document.createElement('div');
-//     container.className = 'container';
-// }
 
 // make new card
 let card = document.createElement('div');
 card.className = 'card';
 document.body.childNodes.item(1).firstChild.appendChild(card);
 
-// add image
+// add image element
 let img = document.createElement("img");
 img.setAttribute('style','width: 100%');
 img.setAttribute('id','cardimg');
 
-if (false) { //add if event contains image
-    img.setAttribute('src', 'assets/borley.png');
+if (false) { // TODO: check if event has image
+    img.setAttribute('src', 'assets/borley.png'); // TODO: change to get event image
 }
 else {
     img.setAttribute('src', 'assets/pawlogo_t.png');
@@ -42,10 +30,12 @@ card.appendChild(container);
 
 // make title
 let title = document.createElement('h1');
-title.innerText = "test";
+title.innerText = "test"; // TODO: change to get event title
 container.appendChild(title);
 
 // make description
 let subtitle = document.createElement('p');
-subtitle.innerText = "subtitle";
+subtitle.innerText = "subtitle"; // TODO: change to get event description
 container.appendChild(subtitle);
+
+// }
