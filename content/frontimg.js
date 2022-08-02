fc39f7c8cfab45a267f9aaa2a793764312d2a30d
HyperionSkunk <cowak186@gmail.com>
2022-08-01 23:27:11 -0500

    Implement getElementById

--- content/frontimg.js
+++ content/frontimg.js
@@ -1,5 +1,5 @@
 var imagesArray = ["barley_ac_1.png", "barley_ac_2.png", "barley_beach_1.png", "barley_mo_carbon_beach_1.png", "mo_beach_1.png", "mo_beach_2.png", "pride_beach_1.png", "sergio_barley_1.png", "twister_ac_1.png", "twister_ac_2.png", "twister_barley_ac_1.png"];
 
-const img = document.querySelectorAll("img");
+const img = document.getElementById("splashimg");
 var num = Math.floor(Math.random() * 10); // 0...11
-img[3].src = '/assets/img/carosel/' + imagesArray[num];
+img.src = '/assets/img/carosel/' + imagesArray[num];
