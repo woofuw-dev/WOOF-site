a74308fe9c299df4acca310b45047f2c69c238d6
HyperionSkunk <cowak186@gmail.com>
2022-08-03 20:01:46 -0500

    update

--- content/events.js
+++ content/events.js
@@ -6,38 +6,38 @@ if (true) { // TODO: check if any events are planned
     // TODO: make new card for each event
     // for (discordEvent in events) {
 
-    // make new card
-    let card = document.createElement('div');
-    card.className = 'card';
-    document.body.childNodes.item(1).firstChild.appendChild(card);
+        // make new card
+        let card = document.createElement('div');
+        card.className = 'card';
+        document.body.childNodes.item(1).firstChild.appendChild(card);
 
-    // add image element
-    let img = document.createElement("img");
-    //img.setAttribute('style', 'width: 20vw');
-    img.setAttribute('id', 'cardimg');
+        // add image element
+        let img = document.createElement("img");
+        //img.setAttribute('style', 'width: 20vw');
+        img.setAttribute('id', 'cardimg');
 
-    if (false) { // TODO: check if event has image
-        img.setAttribute('src', 'assets/borley.png'); // TODO: change to get event image
-    }
-    else { // default for events with no images
-        img.setAttribute('src', 'assets/pawlogo_t.png');
-    }
-    card.appendChild(img);
+        if (false) { // TODO: check if event has image
+            img.setAttribute('src', 'assets/borley.png'); // TODO: change to get event image
+        }
+        else { // default for events with no images
+            img.setAttribute('src', 'assets/pawlogo_t.png');
+        }
+        card.appendChild(img);
 
-    // make text container
-    let container = document.createElement('div');
-    container.className = 'container';
-    card.appendChild(container);
+        // make text container
+        let container = document.createElement('div');
+        container.className = 'container';
+        card.appendChild(container);
 
-    // make title
-    let title = document.createElement('h1');
-    title.innerText = "No title"; // TODO: change to get event title
-    container.appendChild(title);
+        // make title
+        let title = document.createElement('h1');
+        title.innerText = "No title"; // TODO: change to get event title
+        container.appendChild(title);
 
-    // make description
-    let subtitle = document.createElement('p');
-    subtitle.innerText = "No description." // TODO: change to get event description
-    container.appendChild(subtitle);
+        // make description
+        let subtitle = document.createElement('p');
+        subtitle.innerText = "No description." // TODO: change to get event description
+        container.appendChild(subtitle);
 
     // }
 }
