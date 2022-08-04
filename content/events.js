d7ec156bb33c90d3f0e59e36d5f8b567fc636078
HyperionSkunk <cowak186@gmail.com>
2022-08-03 21:31:18 -0500

    Streamlined frontimg.js code and added random images to event cards

--- content/events.js
+++ content/events.js
@@ -1,4 +1,5 @@
 const server_id = '633521381593710602';
+const NUM_CARD_IMAGES = 5;
 var events;
 //get events from server
 
@@ -6,22 +7,29 @@ if (true) { // TODO: check if any events are planned
     // TODO: make new card for each event
     // for (discordEvent in events) {
 
+        let cardcontainer = document.createElement('div');
+        cardcontainer.className = 'cardcontainer';
+        document.body.childNodes.item(1).firstChild.appendChild(cardcontainer);
+
         // make new card
         let card = document.createElement('div');
         card.className = 'card';
-        document.body.childNodes.item(1).firstChild.appendChild(card);
+        cardcontainer.appendChild(card);
 
         // add image element
         let img = document.createElement("img");
         //img.setAttribute('style', 'width: 20vw');
         img.setAttribute('id', 'cardimg');
 
-        if (false) { // TODO: check if event has image
-            img.setAttribute('src', 'assets/borley.png'); // TODO: change to get event image
-        }
-        else { // default for events with no images
-            img.setAttribute('src', 'assets/pawlogo_t.png');
-        }
+        // if (false) { // TODO: check if event has image
+        //     img.setAttribute('src', 'assets/borley.png'); // TODO: change to get event image
+        // }
+        // else { // default for events with no images
+        //     img.setAttribute('src', 'assets/pawlogo_t.png');
+        // }
+        var num = Math.floor(Math.random() * NUM_CARD_IMAGES); // select random image from list
+        //img.src = 'assets/img/cards/card' + num + '.jpg';
+        img.setAttribute('src', 'assets/img/cards/card' + (num + 1) + '.jpg');
         card.appendChild(img);
 
         // make text container
@@ -50,3 +58,10 @@ else { // display "No events planned :(" if no events found
     title.innerText = "No events planned :(";
     noevent.appendChild(title);
 }
+
+
+// //const imagesArray = ["barley_ac_1.png", "barley_ac_2.png", "barley_beach_1.png", "barley_mo_carbon_beach_1.png", "mo_beach_1.png", "mo_beach_2.png", "pride_beach_1.png", "sergio_barley_1.png", "twister_ac_1.png", "twister_ac_2.png", "twister_barley_ac_1.png"];
+// const img = document.getElementById("splashimg");
+
+// var num = Math.floor(Math.random() * imagesArray.length); // select random image from list
+// img.src = 'assets/img/cards/card' + num + '.jpg';
