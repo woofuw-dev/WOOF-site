6d14646f34fe668f98f9f7c205e28422ff61ecb6
HyperionSkunk <cowak186@gmail.com>
2022-08-06 23:49:54 -0500

    Event Calendar Page (#16)
    * updated pug base and index to allow for different splash content
    
    * added beggining of card to page
    
    * continuing card changes
    
    * image revision
    
    * added comments
    
    * clean up base
    
    * center card
    
    * update css
    
    * border radius increased
    
    * css wrapping
    
    * remove testing loop
    
    * define event image width
    
    * remove discord.js dependency
    
    * updated pug base and index to allow for different splash content
    
    * added beggining of card to page
    
    * continuing card changes
    
    * image revision
    
    * added comments
    
    * clean up base
    
    * center card
    
    * update css
    
    * border radius increased
    
    * css wrapping
    
    * remove testing loop
    
    * define event image width
    
    * remove discord.js dependency
    
    * refining card layout
    
    * update
    
    * improve css across multiple pages
    
    * Adjust CSS and hrefs for new Events page
    
    * make min-height for event card better :)
    
    * update
    
    * css adjustments
    
    * Streamlined frontimg.js code and added random images to event cards
    
    * Code touchups
    
    * Modified card CSS to make width independant of image to improve loading
    
    * Removed unused CSS
    
    * Cleaning up CSS Formating slightly
    
    * Further CSS Formatting improvements
    
    * Fixed text alignment
    
    * adding footer
    
    * improved footer
    
    * fixed broken link
    
    * minor footer adjustment
    
    * minor css fix
    
    * First possibly functional iteration of the event page
    
    * Complete first release of events
    
    * Formatting
    
    * Formatting
    
    * Cleaning up unnecessary files
    
    * Meta update
    
    Co-authored-by: by77er <by77er@gmail.com>

--- content/frontimg.js
+++ content/frontimg.js
@@ -1,5 +1,8 @@
-var imagesArray = ["barley_ac_1.png", "barley_ac_2.png", "barley_beach_1.png", "barley_mo_carbon_beach_1.png", "mo_beach_1.png", "mo_beach_2.png", "pride_beach_1.png", "sergio_barley_1.png", "twister_ac_1.png", "twister_ac_2.png", "twister_barley_ac_1.png"];
+const IMAGES_ARRAY = ["barley_ac_1.png", "barley_ac_2.png", "barley_beach_1.png", "barley_mo_carbon_beach_1.png",
+    "mo_beach_1.png", "mo_beach_2.png", "pride_beach_1.png", "sergio_barley_1.png",
+    "twister_ac_1.png", "twister_ac_2.png", "twister_barley_ac_1.png"];
 
-const img = document.getElementById("splashimg");
-var num = Math.floor(Math.random() * imagesArray.length); // select random image from list
-img.src = 'assets/img/carosel/' + imagesArray[num];
+const IMG = document.getElementById("splashimg");
+
+var num = Math.floor(Math.random() * IMAGES_ARRAY.length); // select random image from list
+IMG.src = 'assets/img/carosel/' + IMAGES_ARRAY[num];
