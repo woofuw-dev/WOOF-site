d1f909faa112ce8e8003e12cbeea20dbe5614cef
HyperionSkunk <cowak186@gmail.com>
2022-08-11 13:41:37 -0500

    Spelling is hard

--- content/frontimg.js
+++ content/frontimg.js
@@ -1,6 +1,6 @@
-const NUM_CAROSEL_IMAGES = 12;
+const NUM_CAROUSEL_IMAGES = 12;
 
 const IMG = document.getElementById("splashimg");
 
-var num = Math.floor(Math.random() * NUM_CAROSEL_IMAGES); // select random image from list
-IMG.src = 'assets/img/carosel/carosel' + (num + 1) + '.png';
+var num = Math.floor(Math.random() * NUM_CAROUSEL_IMAGES); // select random image from list
+IMG.src = 'assets/img/carousel/carousel' + (num + 1) + '.png';
