4771593fcee815daf4b016e0191b56b4bb52e9fc
HyperionSkunk <cowak186@gmail.com>
2022-08-11 01:52:38 -0500

    Update image selection and add images

--- content/frontimg.js
+++ content/frontimg.js
@@ -1,8 +1,6 @@
-const IMAGES_ARRAY = ["barley_ac_1.png", "barley_ac_2.png", "barley_beach_1.png", "barley_mo_carbon_beach_1.png",
-    "mo_beach_1.png", "mo_beach_2.png", "pride_beach_1.png", "sergio_barley_1.png",
-    "twister_ac_1.png", "twister_ac_2.png", "twister_barley_ac_1.png"];
+const NUM_CAROSEL_IMAGES = 12;
 
 const IMG = document.getElementById("splashimg");
 
-var num = Math.floor(Math.random() * IMAGES_ARRAY.length); // select random image from list
-IMG.src = 'assets/img/carosel/' + IMAGES_ARRAY[num];
+var num = Math.floor(Math.random() * NUM_CAROSEL_IMAGES); // select random image from list
+IMG.src = 'assets/img/carosel/carosel' + (num + 1) + '.png';
